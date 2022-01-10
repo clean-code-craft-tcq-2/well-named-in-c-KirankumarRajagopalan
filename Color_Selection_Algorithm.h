5f47c28d21dcdde3e2c3eca20ef5a37d641cdfd6
KirankumarRajagopalan <91051522+KirankumarRajagopalan@users.noreply.github.com>
2022-01-10 23:59:47 +0530

    Update Color_Selection_Algorithm.h

--- Color_Selection_Algorithm.h
+++ Color_Selection_Algorithm.h
@@ -1,1 +1,19 @@
+#indef COLOR_SELECTION_ALGORITHM_H
+#define COLOR_SELECTION_ALGORITHM_H
 
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+#endif
