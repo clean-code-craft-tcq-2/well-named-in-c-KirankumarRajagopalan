17ead5a1a7026e26efd18106cb0385e6b86bc32e
KirankumarRajagopalan <91051522+KirankumarRajagopalan@users.noreply.github.com>
2022-01-12 00:32:41 +0530

    Update Color_Selection_Algorithm.h

--- Color_Selection_Algorithm.h
+++ Color_Selection_Algorithm.h
@@ -1,17 +1,17 @@
 #ifndef COLOR_SELECTION_ALGORITHM_H_INCLUDED
 #define COLOR_SELECTION_ALGORITHM_H_INCLUDED
 
-extern const char* MajorColorNames[] = {
+const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
-extern const char* MinorColorNames[] = {
+const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
-extern int numberOfMajorColors =
+int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
-extern int numberOfMinorColors =
+int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 #endif
