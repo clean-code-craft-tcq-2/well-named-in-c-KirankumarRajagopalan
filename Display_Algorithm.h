4e80d9c27340f14a85f1e44b8831255a677a06a7
KirankumarRajagopalan <91051522+KirankumarRajagopalan@users.noreply.github.com>
2022-01-11 23:46:05 +0530

    Update Display_Algorithm.h

--- Display_Algorithm.h
+++ Display_Algorithm.h
@@ -9,6 +9,8 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
+void testNumberToPair( int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
