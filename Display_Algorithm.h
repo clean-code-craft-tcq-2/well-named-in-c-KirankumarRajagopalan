a2029f267ee57d81bbc35e3815d7ca935fae4e7c
KirankumarRajagopalan <91051522+KirankumarRajagopalan@users.noreply.github.com>
2022-01-26 15:34:27 +0530

    Update Display_Algorithm.h

--- Display_Algorithm.h
+++ Display_Algorithm.h
@@ -1,20 +1,6 @@
-#ifndef DISPLAY_ALGORITHM_H_INCLUDED
-#define DISPLAY_ALGORITHM_H_INCLUDED
-
+#include <stdio.h>
+#include <assert.h>
 #include "Color_Selection_Algorithm.h"
-
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
 
 void testNumberToPair( int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
-ColorPair GetColorFromPairNumber(int pairNumber);
-int GetPairNumberFromColor(const ColorPair* colorPair);
-
-#endif
